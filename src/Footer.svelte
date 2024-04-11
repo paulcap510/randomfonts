e304e39d386f3df8373dbfd411759f9666fb1a07
Paul <paulcapob@gmail.com>
2024-04-10 21:30:48 -0400

    Added favicon

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -4,9 +4,9 @@
 
 <div class="footer">
     <p>Copyright &#169; GetRandomFont.com 2024</p>
-
     <p>Email us <a href="mailto:paulcapob@gmail.com">here</a>! Learn more about us here (coming soon).</p> 
     <p>If you really like this app, support us on <a href="https://paypal.me/paulcapdev?country.x=US&locale.x=en_US" target="_blank">PayPal</a>.</p>
+    <p>Favicon by <a href="https://icons8.com/">Icons 8</a></p>
 
 
 </div>
